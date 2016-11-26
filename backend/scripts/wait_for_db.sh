cd6a879b6ac108659815236ba09adbc36a12092d
oprogramador <uomodislesia@gmail.com>
2016-11-26 18:18:49 +0100

    waiting for db

---
+++ backend/scripts/wait_for_db.sh
@@ -0,0 +1,12 @@
+#apt-get update
+apt-get -y install netcat
+until nc -z $DB_HOST $DB_PORT
+do
+  echo waiting for database
+  sleep 1
+done
+for i in `seq 30`
+do
+  echo waiting for database
+  sleep 1
+done
