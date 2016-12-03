9965512fd36748bf1becb0ed24c44bf562f41fde
oprogramador <uomodislesia@gmail.com>
2016-12-03 18:26:07 +0100

    Revert "simulating failing CI"
    This reverts commit 86e9c61291e0f723da5201a80751304f0f4b6061.

--- app/tests/integration/serializer/ArangoSerializer.js
+++ app/tests/integration/serializer/ArangoSerializer.js
@@ -1,5 +1,5 @@
 import ArangoSerializer from 'js-abstract-synchronizer/serializer/ArangoSerializer';
-import {db } from 'js-abstract-synchronizer/servicesManager';
+import { db } from 'js-abstract-synchronizer/servicesManager';
 import runSerializerBasicTests from
   'js-abstract-synchronizer/tests/integration/serializer/helpers/runSerializerBasicTests';
 
